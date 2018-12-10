8df5ae2b05d9722e464b00ad47366fa8866b8699
Aniket Kumar <akumar16@ch.iitr.ac.in>
2018-12-10 13:00:31 +0530

    Resolved syntax issues

--- Lib/Vec2.js
+++ Lib/Vec2.js
@@ -1,60 +1,53 @@
-var Vec2 = function (x, y){
+var Vec2 = function (x, y) {
 	this.x = x;
 	this.y = y;
 };
-
+//returns the length of the vector
 Vec2.prototype.length = function () {
 	return Math.sqrt(this.x * this.x + this.y * this.y);
 };
-
+//returns the vector sum with another vector
 Vec2.prototype.add = function (vec) {
 	return new Vec2(this.x + vec.x , this.y + vec.y);
 };
-
+//returns the vector difference with another vector
 Vec2.prototype.subtract = function (vec) {
 	return new Vec2(this.x - vec.x, this.y - vec.y);
 };
-
+//returns a vector scaled by a given factor
 Vec2.prototype.scale = function (factor) {
 	return new Vec2(this.x * factor, this.y * factor);
 };
+//returns the dot product with another vector
 Vec2.prototype.dot = function (vec) {
 	return (this.x * vec.x + this.y * vec.y);
 };
-
+//returns the magnitude of cross product with another vector
 Vec2.prototype.cross = function (vec) {
 	return (this.x * vec.y - this.y * vec.x);
 };
-
-Vec2.prototype.rotate = function (center, angle){
-	//rotate in counter clock wise
-	var r = [];
+//returns a vector rotated by <angle> radians counter clock wise
+Vec2.prototype.rotate = function (center, angle) {
+	var r = new Vec2(0,0);
 	var x = this.x - center.x;
 	var y = this.y - center.y;
-	r[0] = x * Math.cos(angle) - y * Math.sin(angle);
-	r[1] = x * Math.sin(angle) + y * Math.cos(angle);
-	r[0] += center.x;
-	r[1] += center.y;
-	return new Vec2(r[0], r[1]);
+	r.x = x * Math.cos(angle) - y * Math.sin(angle);
+	r.y = x * Math.sin(angle) + y * Math.cos(angle);
+	r.x += center.x;
+	r.y += center.y;
+	return r;
 };
-
+//returns a unit vector pointing towards the vector's direction
 Vec2.prototype.normalize = function () {
 	var len = this.length();
 	if ( len > 0 ){
-		len = 1/len;
+		len = 1.0/len;
 	}
 	return this.scale(len);
 };
-
-Vec2.prototype.distance = function (vec){
+//returns the distance between this and another point
+Vec2.prototype.distance = function (vec) {
 	var x = this.x - vec.x;
 	var y = this.y - vec.y;
 	return Math.sqrt( x * x + y * y );
 };
-
-
-
-
-
-
-
