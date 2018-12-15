7d3c37f619b38d32f0f36379f48ddd0ea02665c2
Aniket Kumar <akumar16@ch.iitr.ac.in>
2018-12-15 18:10:04 +0530

    Added the update function

--- RigidBody/RigidShape.js
+++ RigidBody/RigidShape.js
@@ -3,6 +3,12 @@
 function RigidShape(center) {
 	this.mCenter = center;
 	this.mAngle = 0;
-	
+		
 	gEngine.Core.mAllObjects.push(this);
 }
+
+//Update the parameters
+RigidShape.prototype.update = function (){
+	if(this.mCenter.y < gEngine.Core.mHeight && this.mFix != 0)
+		this.move(new Vec2(0, 1));
+};
